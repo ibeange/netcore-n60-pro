fd0f88b8550a703591c65d163dbab4f578d9c63f
Ethan <word.of.ethan@gmail.com>
2025-11-14 19:37:13 +0800

    Update diy-part5-6.6.sh

--- diy-part5-6.6.sh
+++ diy-part5-6.6.sh
@@ -198,9 +198,9 @@ sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7
 echo
 status "菜单 调整..."
 sed -i 's|/services/|/control/|' feeds/luci/applications/luci-app-wol/root/usr/share/luci/menu.d/luci-app-wol.json
-sed -i 's/services/nas/g' package/luci-app-openlist2/luci-app-openlist2/root/usr/share/luci/menu.d/luci-app-openlist2.json
-sed -i '/"title": "Nikki",/a \        "order": -9,' package/OpenWrt-nikki/luci-app-nikki/root/usr/share/luci/menu.d/luci-app-nikki.json
-sed -i 's/("OpenClash"), 50)/("OpenClash"), -10)/g' package/luci-app-openclash/luci-app-openclash/luasrc/controller/openclash.lua
+sed -i 's/services/nas/g' package/luci-app-openlist2/root/usr/share/luci/menu.d/luci-app-openlist2.json
+sed -i '/"title": "Nikki",/a \        "order": -9,' package/luci-app-nikki/root/usr/share/luci/menu.d/luci-app-nikki.json
+sed -i 's/("OpenClash"), 50)/("OpenClash"), -10)/g' package/luci-app-openclash/luasrc/controller/openclash.lua
 sed -i 's/"网络存储"/"存储"/g' `grep "网络存储" -rl ./`
 sed -i 's/"软件包"/"软件管理"/g' `grep "软件包" -rl ./`
 
