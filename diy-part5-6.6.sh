73fc9070d6461376071e4cffb8bffa8bbea5c339
Ethan <word.of.ethan@gmail.com>
2025-11-15 07:09:24 +0800

    Update diy-part5-6.6.sh

--- diy-part5-6.6.sh
+++ diy-part5-6.6.sh
@@ -220,9 +220,10 @@ mv package/small/uugamebooster package/uugamebooster
 mv package/small/luci-app-uugamebooster package/luci-app-uugamebooster
 
 # openlist2
-rm -rf feeds/packages/net/openlist
-rm -rf feeds/luci/applications/luci-app-openlist
-git clone https://github.com/OpenListTeam/OpenList-OpenWRT package/openlist
+rm -rf feeds/packages/net/openlist2
+rm -rf feeds/luci/applications/luci-app-openlist2
+mv package/small/openlist2 package/openlist2
+mv package/small/luci-app-openlist2 package/luci-app-openlist2
 sed -i 's/services/nas/g' package/openlist/luci-app-openlist/root/usr/share/luci/menu.d/luci-app-openlist.json
 sed -i 's/"title": "OpenList",/&\n        "order": 0,/g' package/openlist/luci-app-openlist/root/usr/share/luci/menu.d/luci-app-openlist.json
 
